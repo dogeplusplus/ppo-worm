25b753a955462a46db7e93cfd54ad28489e2520a
dogeplusplus <albertswchung@gmail.com>
2022-01-30 09:54:17 +0000

    Format agent code better.

--- ppo_worm/agent.py
+++ ppo_worm/agent.py
@@ -35,7 +35,7 @@ class PPOCategoricalActor(PPOActor):
     def __init__(self, obs_dim, act_dim, hidden_sizes, activation):
         super().__init__()
         self.logits_net = mlp([obs_dim] + list(hidden_sizes) + [act_dim], activation)
-    
+
     def _distribution(self, obs):
         logits = self.logits_net(obs)
         return Categorical(logits=logits)
@@ -56,7 +56,7 @@ class PPOGaussianActor(PPOActor):
         mu = self.mu_net(obs)
         std = torch.exp(self.log_std)
         return Normal(mu, std)
-    
+
     def _log_prob_from_distribution(self, pi, act):
         return pi.log_prob(act).sum(axis=-1)
 
@@ -70,13 +70,13 @@ class PPOCritic(nn.Module):
     def forward(self, obs):
         # To ensure v has the right shape
         return torch.squeeze(self.v_net(obs), -1)
-        
+
 
 class PPOActorCritic(nn.Module):
 
     def __init__(self, observation_space, action_space, hidden_sizes=(64, 64), activation=nn.Tanh):
         super().__init__()
-        
+
         # Unity vs Gym
         if isinstance(observation_space, ObservationSpec):
             obs_dim = observation_space.shape
@@ -105,4 +105,4 @@ class PPOActorCritic(nn.Module):
 
     def act(self, obs):
         return self.step(obs)[0]
-        
+
